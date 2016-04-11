5aa78e12b0ef6925f607aa672984decd962c3014
dgmoney11 <girarddylan1207@gmail.com>
2016-04-11 13:32:24 -0400

    Update StartMenu.java

--- StartMenu.java
+++ StartMenu.java
@@ -3,6 +3,7 @@ package model;
 
 /**
  * <!-- begin-user-doc -->
+ * Starting point in the application. Holds objects for the other classes used in this software.
  * <!--  end-user-doc  -->
  * @generated
  */
@@ -11,91 +12,113 @@ public class StartMenu
 {
 	/**
 	 * <!-- begin-user-doc -->
+	 * Allows the user to access the menu editor from within the start menu.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public MenuEditor menuEditor;
+	private MenuEditor menuEditor;
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Allows the user to make a new order from the start menu
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public NewOrder newOrder;
+	private NewOrder newOrder;
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Allows the user to view the statistics page from the start menu
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public StatsPage statsPage;
+	private StatsPage statsPage;
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Allows the user to access the Kitchen view from the start menu.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public KitchenView kitchenView;
+	private KitchenView kitchenView;
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Create a StartMenu object.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 */
 	public StartMenu(){
 		super();
+		createMenuEditor();
+		createNewOrder();
+		createStatsPage();
+		createKitchenView();
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Will call the MenuEditor constructor, as long as it hasn't been instantiated yet.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
 	public void createMenuEditor() {
-		// TODO implement me
+		if(this.menuEditor == null)
+		{
+			menuEditor = new MenuEditor();
+		}
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Will create a NewOrder object, a new order must be made every time it is accessed from the start menu.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
 	public void createNewOrder() {
-		// TODO implement me
+		newOrder = NewOrder();
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Will call the StatsPage constructor, as long as it hasn't been instantiated yet.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
 	public void createStatsPage() {
-		// TODO implement me
+		if(this.newOrder == null)
+		{
+			mewOrder = NewOrder();
+		}
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Will call the StatsPage constructor, as long as it hasn't been instantiated yet.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
 	public void createKitchenView() {
-		// TODO implement me
+		if(this.kitchenView == null)
+		{
+			kitchenView = new KitchenView();
+		}
 	}
 
 }
