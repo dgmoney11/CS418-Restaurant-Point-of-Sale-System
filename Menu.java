7dbe0f70a81a36c4006fd3305586d5e5a2885018
dgmoney11 <girarddylan1207@gmail.com>
2016-04-11 10:03:47 -0400

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -12,15 +12,19 @@ public class Menu
 {
 	/**
 	 * <!-- begin-user-doc -->
+	 * A list of categories, Each category holds a list of items.
+	 * A category for this software would be like 
+	 * breakfast, lunch, dinner, etc..
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public Category categoryArray;
+	private Category categoryArray[];
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Constructor to create a Category object.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 */
@@ -30,13 +34,14 @@ public class Menu
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Initialize the categories to the number in the int parameter.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
 	public void initializeCategories(int parameter) {
-		// TODO implement me
+		this.categoryArray = new categoryArray[parameter];
 	}
 
 }
