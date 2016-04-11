6c1cb115e839e3a833fa3f5f8b7b262eb970ea94
dgmoney11 <girarddylan1207@gmail.com>
2016-04-11 11:29:51 -0400

    Update NewOrder.java

--- NewOrder.java
+++ NewOrder.java
@@ -1,8 +1,8 @@
 package model;
 
-
 /**
  * <!-- begin-user-doc -->
+ * Whenever wait staff place an order, a NewOrder object is instantiated.
  * <!--  end-user-doc  -->
  * @generated
  */
@@ -11,71 +11,130 @@ public class NewOrder
 {
 	/**
 	 * <!-- begin-user-doc -->
+	 * List of items to add to the order.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public MenuItem itemList;
+	private MenuItem itemList[];
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Total price of the order.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public int totalPrice;
+	private int totalPrice;
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * A comment for an order, an example would be no onions on a burger.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public String commentList;
+	private String commentList[];
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Constructor for a NewOrder object. Initializes data fields.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 */
 	public NewOrder(){
 		super();
+		this.itemList = new MenuItem[5];
+		this.commentList = new String[5];
+		this.totalPrice = 0;
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Add a comment for an order. If the array is full then copy the data to a new array and double
+	 * the size of the array.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
 	public void addComment(String comment) {
-		// TODO implement me
+		if(this.commentList[commentList.length-1] == null)
+		{
+			for(int i = 0; i < this.commentList.length; i++)
+			{
+				if(commentList[i] == null)
+				{
+					this.commentList[i] = comment;
+					break;
+				}
+			}
+		}
+		else
+		{
+			String newList = new String[this.commentList.length * 2];
+			for(int i = 0; i < this.commentList.length; i++)
+			{
+				newList[i] = this.commentList[i];
+			}
+			newList[i] = comment;
+			this.commentList = newList;
+		}
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Remove a comment from an item, all this will do is set the comment for that index equal to
+	 * an empty string to indicate that there is an item that corresponds to that index.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
 	public void removeComment(String comment) {
-		// TODO implement me
+		for(int i = 0; i < commentList.length; i++)
+		{
+			if(commentList[i].equals(comment))
+			{
+				commentList[i] = "";
+			}
+		}
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Will follow a similar logic to the add comment method, only this function will add 
+	 * a MenuItem object to the order.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
 	public void addToOrder(MenuItem item) {
-		// TODO implement me
+		if(this.itemList[itemList.length-1] == null)
+		{
+			for(int i = 0; i < this.itemList.length; i++)
+			{
+				if(itemList[i] == null)
+				{
+					this.itemList[i] = item;
+					break;
+				}
+			}
+		}
+		else
+		{
+			String newList = new String[this.itemList.length * 2];
+			for(int i = 0; i < this.itemList.length; i++)
+			{
+				newList[i] = this.itemList[i];
+			}
+			newList[i] = item;
+			this.itemList = newList;
+		}
 	}
 
 	/**
@@ -91,36 +150,39 @@ public class NewOrder
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Adds to the total the cost of an item added to the order.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public void addToTotal(int orderTotal) {
-		// TODO implement me
+	public void addToTotal(int cost) {
+		this.totalPrice += cost;
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Removes from the total price of the order the dollar amount 
+	 * that is equal to the integer argument.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public void removeFromTotal(int orderTotal) {
-		// TODO implement me
+	public void removeFromTotal(int cost) {
+		this.totalPrice -= cost;
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Returns the total cost of the order.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
 	public int getTotalCost() {
-		// TODO implement me
-		return 0;
+		return totalPrice;
 	}
 
 	/**
