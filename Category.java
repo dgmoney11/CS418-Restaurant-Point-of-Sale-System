a669fd33c8489c48224e3998589f74176cd6b3cf
dgmoney11 <girarddylan1207@gmail.com>
2016-04-11 10:16:25 -0400

    Update Category.java

--- Category.java
+++ Category.java
@@ -3,6 +3,7 @@ package model;
 
 /**
  * <!-- begin-user-doc -->
+ * A category in a menu. Each category has a name and a list of items.
  * <!--  end-user-doc  -->
  * @generated
  */
@@ -11,24 +12,29 @@ public class Category
 {
 	/**
 	 * <!-- begin-user-doc -->
+	 * List of items that belong to a category.
+	 * In this case a category could be breakfast,
+	 * and itemList could contain eggs, toast, and bacon.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public MenuItem itemList;
+	private MenuItem itemList[];
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * The name of the category, such as breakfast.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public String categoryName;
+	private String categoryName;
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Constructor to create a Category object.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 */
@@ -38,6 +44,7 @@ public class Category
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Returns the category name.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
@@ -45,29 +52,31 @@ public class Category
 	
 	public String getCategoryName() {
 		// TODO implement me
-		return "";
+		return this.categoryName;
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Assigns the category name based on the String argument.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public void setCategoryName(String parameter) {
-		// TODO implement me
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Initialize the item list to the number of items specified by the integer argument.
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 * @ordered
 	 */
 	
-	public void initializeItems(int parameter) {
-		// TODO implement me
+	public void initializeItems(int numItems) {
+		this.itemList = new int[numItems];
 	}
 
 }
